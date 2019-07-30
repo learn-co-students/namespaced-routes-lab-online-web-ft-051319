a939961b6a700ce3832b59c6cb40009ba00bddf8
haleydek <hcd09@live.com>
2019-07-30 16:52:46 -0500

    Done.

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -1,14 +1,14 @@
 class Admin::PreferencesController < ApplicationController
-    before_action :set_preferences, only: [:edit, :update]
-
     def index
         @preference = Preference.first
     end
 
     def edit
+        @preference = Preference.first
     end
 
     def update
+        @preference = Preference.find(params[:id])
         if @preference.update(preference_params)
             redirect_to admin_preferences_path
         else
@@ -20,9 +20,5 @@ class Admin::PreferencesController < ApplicationController
 
     def preference_params
         params.require(:preference).permit(:artist_sort_order, :song_sort_order, :allow_create_artists, :allow_create_songs)
-    end
-
-    def set_preferences
-        @preference = Preference.first
     end
 end
